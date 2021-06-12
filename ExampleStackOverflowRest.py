6dc5edacdb84361952eccbd5e919d03f6a50d25b
Chong Jin Yi <chong601.13@gmail.com>
2021-06-12 10:45:00 +0800

    Modify ExampleStackOverflowRest.py - Add new crow_id with stringified UUID4 datatype as a common ID to represent all responses and to satisfy SQLAlchemy requirement of at least one primary key as well as to work around composite primary key limitations exist on SQLite - Remove primary key on response_id in favor for row_id - Add pydoc documentation for each database object - Remove inaccurate comment on enforcing data population if the data doesn't exist on the referenced column

--- ExampleStackOverflowRest.py
+++ ExampleStackOverflowRest.py
@@ -2,9 +2,11 @@ from flask import Flask, jsonify, render_template
 from flask.views import MethodView
 from flask_sqlalchemy import SQLAlchemy
 from dataclasses import dataclass
-from sqlalchemy import Column, JSON, Integer, ForeignKey
+from sqlalchemy import Column, JSON, Integer, ForeignKey, Text
 from flask_migrate import Migrate
 import os
+from uuid import UUID
+from uuid import uuid4
 
 # Create a new Flask instance
 app = Flask(__name__)
@@ -25,6 +27,9 @@ migrate.init_app(app, db)
 # Full schema operation will be built later.
 @dataclass(order=True)
 class Schema(db.Model, object):
+    """
+    Schema database object that represents the 'stackoverflow_schema table in database
+    """
     __tablename__ = 'stackoverflow_schema'
     year: int = Column(Integer, primary_key=True, autoincrement=False,
                        comment='The year of the row the data represented')
@@ -42,12 +47,24 @@ class Schema(db.Model, object):
 # Respondent database model
 @dataclass(order=True)
 class Response(db.Model, object):
+    """
+    Response class object that represents the `stackoverflow_response` table in database
+    """
     __tablename__ = 'stackoverflow_response'
+    # Use UUID to represent the common set of response data on response
+    # This is a workaround to support SQLite as the original intention is to have composite
+    # key that includes response_id and year to make the data unique.
+    # Most database systems out there would generally support composite keys as a primary key
+    # but that's not a choice as it would require a different approach to make it compatible across
+    # all database software out there which is beyond of my skillset as of now.
+    # UUID will be stored as a string again to work around SQLite lack of native UUID data type support
+    # so string version of UUID is selected to maintain compatibility across database software.
+    row_id: UUID = Column(Text, primary_key=True, comment='Common ID of the response', default=str(uuid4()))
     # An id for referring to the data, or extract the respondent ID from the response,
     # which is you know, CRAP.
-    response_id: int = Column(Integer, primary_key=True, comment='The anonymized ID of the response')
+    response_id: int = Column(Integer, comment='The anonymized ID of the response')
     # Set the year column to have a foreign key relationship with the
-    # schema year column. Also enforce to populate the data if it doesn't exist
+    # schema year column.
     year: int = Column(Integer, ForeignKey('stackoverflow_schema.year'), nullable=False)
     # Store responses as JSON because you know, it's goddamn impossible to store the responses using
     # RDBMS concepts. Just use JSON and refer to the schema to extract the data and call it a day, really

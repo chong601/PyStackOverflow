689b6adcc1467a53b3eb2b759da9a2c70ad2dea8
Chong Jin Yi <chong601.13@gmail.com>
2021-06-12 18:24:34 +0800

    Refactor StackOverflowDataDumper.py Enough refactor that I don't even know what am I refactoring the code for tbh, but I know that there's a ton of rewrites for: - rewriting insert throttling (not accounting SSDs) - add tons of logging - misc stuff that I can't think of (I'm tired OK, don't judge me.)

--- StackOverflowDataDumper.py
+++ StackOverflowDataDumper.py
@@ -4,14 +4,15 @@ import logging
 import time
 # Loggy bits
 log = logging.getLogger('__name__')
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 log.addHandler(ch)
 DIR = 'stackoverflow_data'
-
-dump_rate = 10000
-log.info('Begin processing schema data located at "{DIR}".')
+all_op_start_time = time.time_ns()
+total_data = 0
+dump_rate = 1000
+log.info(f'Begin processing schema data located at "{DIR}".')
 for i in range(2017, 2021):
     log.info(f'Start processing StackOverflow schema data for year {str(i)}.')
     with open('/'.join([DIR, str(i), 'survey_results_schema.csv']), newline='',
@@ -25,59 +26,71 @@ for i in range(2017, 2021):
             k, v = d.keys()
             row.update({d[k]: d[v]})
             log.debug(f'Extracted data with key {k} and value {v}')
-        log.info(f'Schema data extraction for year {str(i)} completed.')
-        log.info(f'Saving extracted schema data for year {str(i)} to database...')
+        log.debug(f'Schema data extraction for year {str(i)} completed.')
+        log.debug(f'Saving extracted schema data for year {str(i)} to database...')
         log.debug(f'Begin creating schema object with data {row}')
         schema = Schema(i, row)
         log.debug(f'Adding schema object {schema}...')
         db.session.add(schema)
         log.debug('Schema object created successfully.')
-        log.debug(f'Committing data to database...')
-        db.session.commit()
-        log.debug(f'Commit data to database successful.')
-        log.info(f'Schema data for year {str(i)} extracted.')
+    log.debug(f'Committing data to database...')
+    db.session.commit()
+    log.debug(f'Commit data to database successful.')
+    log.info(f'Schema data for year {str(i)} extracted.')
 log.info('Finished processing all schema data')
 
-log.info('Start processing response data from {DIR}.')
-for i in range(2017,2021):
+previous_time = 0
+
+log.info(f'Start processing response data from {DIR}.')
+for i in range(2017, 2021):
+    count = 0
+    next_check = dump_rate
     log.info(f'Start processing StackOverflow response data for year {str(i)}.')
+
     with open('/'.join([DIR, str(i), 'survey_results_public.csv']), newline='',
               encoding='utf8') as file:
         log.info(f'Response data "{file.name}" opened successfully.')
         data = csv.DictReader(file)
-        log.info(f'Extracting response data from "{file.name}". This make take a long time.')
-        count = 1
-        previous_time=0
+        log.info(f'Extracting response data from "{file.name}". This may take a long time.')
+
         for d in data:
             row = {}
             log.debug(f'Discovered data {d}')
             log.debug(f'Extracted data with key {k} and value {v}')
-            log.info(f'Saving extracted response data for year to database...')
+
             log.debug(f'Begin creating response object with data {row}')
             response = Response(d['Respondent'], i, d)
             log.debug(f'Response object created with data {response}.')
             log.debug(f'Begin saving response data {response} into database...')
             db.session.add(response)
             log.debug('Response data saved.')
-            count += 1
-            if count % dump_rate == 0:
-                log.debug(f'Dump rate is {dump_rate}. Inserted {count} rows of data. It took {previous_time} ns to dump.')
+
+            if count == next_check:
                 log.debug('Committing changes to the database...')
                 start_time = time.time_ns()
                 db.session.commit()
                 end_time = time.time_ns()
-
-                if end_time - start_time > 3*(10**9):
-                    dump_rate -= 500
-                    if dump_rate < 100:
-                        dump_rate = 100
-                elif end_time - start_time < 3*(10**9):
-                    dump_rate += 200
                 log.debug('Commit success.')
+                previous_time = end_time - start_time
+                log.info(
+                    f'Dump rate is {dump_rate}. Inserted {count} rows of data. It took {round(previous_time / 1000000000, 3)}s to commit.')
+                if dump_rate < 500:
+                    dump_rate = 1000
+                    log.info(f'Dump rate limited to {dump_rate} rows.')
+                elif end_time - start_time > 10*(10**9):
 
+                    dump_rate -= 1000 if dump_rate != 0 else 1000
+                    log.info(f'Commit time took longer than 10 seconds. Decreasing dump rate to {dump_rate} rows...')
+                elif end_time - start_time < 10*(10**9):
+                    dump_rate += 500
+                    log.info(f'Commit time is faster than 10 seconds. Increasing dump rate to {dump_rate} rows...')
+                next_check = count + dump_rate
+            count += 1
+        log.info(f'Loaded {count} responses for year {str(i)}')
         log.debug('Committing changes to the database...')
         db.session.commit()
         log.debug('Commit success.')
         log.info(f'Response data for year {str(i)} completed.')
-
-log.info('Finished processing all response data.')
+        total_data += count
+all_op_end_time = time.time_ns()
+log.info(f'Finished processing all response data. Loaded {total_data} rows in {round((all_op_end_time-all_op_start_time)/1000000000, 3)}s.')

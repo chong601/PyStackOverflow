088a6917ccb1d569b98714a6ab6d0ce73745afcb
Chong Jin Yi <chong601.13@gmail.com>
2021-06-13 19:21:23 +0800

    REEEEEEEEfacyor ExampleStackOverflowRest.py - Add more URL rules - Code cleanups - Enable pagination support - Rename column names

--- ExampleStackOverflowRest.py
+++ ExampleStackOverflowRest.py
@@ -17,6 +17,8 @@ db.init_app(app)
 migrate = Migrate(app, db)
 migrate.init_app(app, db)
 
+MAX_RESULTS_PER_PAGE = 100
+
 
 # Schema database model
 # Preferably, this would be based on the schema file provided by StackOverflow,
@@ -30,9 +32,13 @@ class Schema(db.Model, object):
     Schema database object that represents the 'stackoverflow_schema table in database
     """
     __tablename__ = 'stackoverflow_schema'
-    year: int = Column(Integer, primary_key=True, autoincrement=False,
+    insight_year: int = Column(Integer, primary_key=True, autoincrement=False,
                        comment='The year of the row the data represented')
-    columns: dict = Column(JSON, comment='The columns represented by the year', nullable=False)
+    response_columns: dict = Column(JSON, comment='The columns represented by the year', nullable=False)
+
+    def __init__(self, insight_year, response_columns):
+        self.insight_year = insight_year
+        self.response_columns = response_columns
 
     def __init__(self, year, columns):
         self.year = year
@@ -45,7 +51,7 @@ class Schema(db.Model, object):
 
 # Respondent database model
 @dataclass(order=True)
-class Response(db.Model, object):
+class Response(db.Model):
     """
     Response class object that represents the `stackoverflow_response` table in database
     """
@@ -58,22 +64,25 @@ class Response(db.Model, object):
     # all database software out there which is beyond of my skillset as of now.
     # UUID will be stored as a string again to work around SQLite lack of native UUID data type support
     # so string version of UUID is selected to maintain compatibility across database software.
-    row_id: str = Column(Text, primary_key=True, comment='Common ID of the response')
+    id: str = Column(Text, primary_key=True, comment='Common ID of the response')
     # An id for referring to the data, or extract the respondent ID from the response,
     # which is you know, CRAP.
     response_id: int = Column(Integer, comment='The anonymized ID of the response')
     # Set the year column to have a foreign key relationship with the
     # schema year column.
-    year: int = Column(Integer, ForeignKey('stackoverflow_schema.year'), nullable=False)
+    response_year: int = Column(Integer, ForeignKey('stackoverflow_schema.insight_year'), nullable=False)
     # Store responses as JSON because you know, it's goddamn impossible to store the responses using
     # RDBMS concepts. Just use JSON and refer to the schema to extract the data and call it a day, really
     responses: dict = Column(JSON, comment='The responses in JSON corresponding to the year of the data',
                              nullable=False)
 
-    def __init__(self, response_id, year, responses):
-        self.row_id = str(uuid.uuid4())
+    def __init__(self, response_id, response_year, responses, id=None):
+        if id is None:
+            self.id = str(uuid.uuid4())
+        else:
+            self.id = id
         self.response_id = response_id
-        self.year = year
+        self.response_year = response_year
         self.responses = responses
 
     # Don't know if this is doable, but it would be a good debugging tool to start with ;)
@@ -84,19 +93,27 @@ class Response(db.Model, object):
 # Class to provide REST interface for consumption
 class RespondentAnswers(MethodView):
 
-    def get(self, response_id=None):
-        if response_id is None:
-            return jsonify(Response.query.all())
-        elif response_id is int:
-            return jsonify(Response.query.filter_by(response_id=response_id).first_or_404())
+    # TODO: separate get request to respective routes
+    # TODO: pagination
+    # TODO: ratelimit
+    def get(self, response_year=None, response_id=None, page=1):
+        if response_year is None and response_id is None:
+            return jsonify(Response.query.paginate(page=page, per_page=MAX_RESULTS_PER_PAGE, error_out=False).items)
+        elif response_year is not None and response_id is None:
+            return jsonify(Response.query.filter_by(response_year=response_year).paginate(page=page, per_page=MAX_RESULTS_PER_PAGE, error_out=False).items)
+        elif response_year is not None and response_id is not None:
+            return jsonify(
+                Response.query.filter_by(response_year=response_year, response_id=response_id).first_or_404())
         return jsonify({'error': 'Invalid request'}), 400
 
 
 # Requires passing in JSON data
-app.add_url_rule('/response', methods=['GET'], view_func=RespondentAnswers.as_view('users'))
+app.add_url_rule('/responses/<int:page>', methods=['GET'], view_func=RespondentAnswers.as_view('users'))
 # Only require the ID for the operation
-app.add_url_rule('/response/<int:response_id>', methods=['GET'],
-                 view_func=RespondentAnswers.as_view('users_id'))
+app.add_url_rule('/responses/<int:response_year>/<int:page>', methods=['GET'],
+                 view_func=RespondentAnswers.as_view('users_year'))
+app.add_url_rule('/response/<int:response_year>/<int:response_id>', methods=['GET'],
+                 view_func=RespondentAnswers.as_view('users_year_id'))
 
 if __name__ == '__main__':
     app.run(use_reloader=False)

d9f4390a33cbf2dd4e1d764cd0da209ea6fd52c1
Chong Jin Yi <chong601.13@gmail.com>
2021-06-14 17:36:57 +0800

    [PATCH 02/03] Refactor ExampleStackOverflowRest.py - Disable MethodView-based views - Use per-function view and add routes for     - get all response data with pagination support     - get all response data by year with pagination support     - get response data by response ID     - get response data by year and respondent ID

--- ExampleStackOverflowRest.py
+++ ExampleStackOverflowRest.py
@@ -1,6 +1,6 @@
 import dataclasses
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, request
 from flask.views import MethodView
 from flask_sqlalchemy import SQLAlchemy
 from dataclasses import dataclass
@@ -34,18 +34,18 @@ MAX_RESULTS_PER_PAGE = 100
 # complicates the implementation a bit.
 # This class exists purely to facilitate data transfer to and from database.
 # Full schema operation will be built later.
-@dataclass(order=True)
+@dataclass
 class Schema(db.Model, object):
     """
     Schema database object that represents the 'stackoverflow_schema table in database
     """
     __tablename__ = 'stackoverflow_schema'
-    insight_year: int = Column(Integer, primary_key=True, autoincrement=False,
+    year: int = Column(Integer, primary_key=True, autoincrement=False,
                        comment='The year of the row the data represented')
     response_columns: dict = Column(JSON, comment='The columns represented by the year', nullable=False)
 
-    def __init__(self, insight_year, response_columns):
-        self.insight_year = insight_year
+    def __init__(self, year, response_columns):
+        self.year = year
         self.response_columns = response_columns
 
     @staticmethod
@@ -55,7 +55,8 @@ class Schema(db.Model, object):
 
         :return: Dictionary of mappable attributes
         """
-        keys = {'insight_year', 'response_columns'}
+        keys = {'year', 'response_columns'}
+        return keys
 
     # Don't know if this is doable, but it would be a good debugging tool to start with ;)
     def __repr__(self):
@@ -74,7 +75,7 @@ class OrderedClassMembers(type):
 
 
 # Respondent database model
-@dataclass(order=True)
+@dataclass
 class Response(db.Model):
     """
     Response class object that represents the `stackoverflow_response` table in database
@@ -88,24 +89,24 @@ class Response(db.Model):
     # all database software out there which is beyond of my skillset as of now.
     # UUID will be stored as a string again to work around SQLite lack of native UUID data type support
     # so string version of UUID is selected to maintain compatibility across database software.
-    id: str = Column(Text, primary_key=True, comment='Common ID of the response')
+    response_id: str = Column(Text, primary_key=True, comment='Common ID of the response')
     # An id for referring to the data, or extract the respondent ID from the response,
     # which is you know, CRAP.
-    response_id: int = Column(Integer, comment='The anonymized ID of the response')
+    respondent_id: int = Column(Integer, comment='The anonymized ID of the response')
     # Set the year column to have a foreign key relationship with the
     # schema year column.
-    response_year: int = Column(Integer, ForeignKey('stackoverflow_schema.insight_year'), nullable=False)
+    response_year: int = Column(Integer, ForeignKey('stackoverflow_schema.year'), nullable=False)
     # Store responses as JSON because you know, it's goddamn impossible to store the responses using
     # RDBMS concepts. Just use JSON and refer to the schema to extract the data and call it a day, really
     responses: dict = Column(JSON, comment='The responses in JSON corresponding to the year of the data',
                              nullable=False)
 
-    def __init__(self, response_id, response_year, responses, id=None):
-        if id is None:
-            self.id = str(uuid.uuid4())
+    def __init__(self, respondent_id, response_year, responses, response_id=None):
+        if response_id is None:
+            self.response_id = str(uuid.uuid4())
         else:
-            self.id = id
-        self.response_id = response_id
+            self.response_id = response_id
+        self.respondent_id = respondent_id
         self.response_year = response_year
         self.responses = responses
 
@@ -116,36 +117,74 @@ class Response(db.Model):
 
         :return: Dictionary of mappable attributes
         """
-        keys = {'id', 'response_id', 'response_year', 'responses'}
+        keys = {'response_id', 'respondent_id', 'response_year', 'responses'}
+        return keys
 
     def __repr__(self):
         return f'{self.__class__.__name__}({vars(self)})'
 
 
+# TODO: get rid of MethodView (not suitable for this use case as we only do GET requests
+@app.route('/responses')
+def get_response_per_page():
+    page_number = request.args.get('page', 1, type=int)
+    size_per_page = request.args.get('size', MAX_RESULTS_PER_PAGE, type=int)
+    result = Response.query.paginate(page=page_number, per_page=size_per_page, error_out=False).items
+    return (jsonify({'error': 'Database is empty'}), 404) if len(result) == 0 else jsonify(result)
+
+
+@app.route('/responses/<int:year>')
+def get_response_by_year_per_page(year):
+    page_number = request.args.get('page', 1, type=int)
+    size_per_page = request.args.get('size', MAX_RESULTS_PER_PAGE, type=int)
+    result = Response.query.filter_by(response_year=year).paginate(page=page_number, per_page=size_per_page, error_out=False).items
+    return (jsonify({'error': 'Database is empty'}), 404) if len(result) == 0 else jsonify(result)
+
+
+@app.route('/response/<response_id>')
+def get_response_by_response_id(response_id):
+    # response_id = request.args.get('response_id', type=str)
+    result = Response.query.filter_by(response_id=response_id).all()
+    if len(result) > 1:
+        result.append({'Warning': 'More than one response detected!'})
+    return (jsonify({'error': f'Response ID {response_id} not found.'}), 404) \
+        if len(result) != 1 else jsonify(result)
+
+
+@app.route('/response/<int:year>/<int:respondent_id>')
+def get_response_by_year_respondent_id(year, respondent_id):
+    # year = request.args.get('year', type=int)
+    # respondent_id = request.args.get('respondent_id', type=int)
+    result = Response.query.filter_by(response_year=year, respondent_id=respondent_id).all()
+    return (jsonify({'error': f'Response data for respondent ID {respondent_id} for year {year} is not found.'}), 404) \
+        if len(result) != 1 else jsonify(result)
+
+
+
 # Class to provide REST interface for consumption
-class RespondentAnswers(MethodView):
-
-    # TODO: separate get request to respective routes
-    # TODO: pagination
-    # TODO: ratelimit
-    def get(self, response_year=None, response_id=None, page=1):
-        if response_year is None and response_id is None:
-            return jsonify(Response.query.paginate(page=page, per_page=MAX_RESULTS_PER_PAGE, error_out=False).items)
-        elif response_year is not None and response_id is None:
-            return jsonify(Response.query.filter_by(response_year=response_year).paginate(page=page, per_page=MAX_RESULTS_PER_PAGE, error_out=False).items)
-        elif response_year is not None and response_id is not None:
-            return jsonify(
-                Response.query.filter_by(response_year=response_year, response_id=response_id).first_or_404())
-        return jsonify({'error': 'Invalid request'}), 400
-
-
-# Requires passing in JSON data
-app.add_url_rule('/responses/<int:page>', methods=['GET'], view_func=RespondentAnswers.as_view('users'))
-# Only require the ID for the operation
-app.add_url_rule('/responses/<int:response_year>/<int:page>', methods=['GET'],
-                 view_func=RespondentAnswers.as_view('users_year'))
-app.add_url_rule('/response/<int:response_year>/<int:response_id>', methods=['GET'],
-                 view_func=RespondentAnswers.as_view('users_year_id'))
+# class RespondentAnswers(MethodView):
+#
+#     # TODO: separate get request to respective routes
+#     # TODO: pagination
+#     # TODO: ratelimit
+#     def get(self, response_year=None, response_id=None, page=1):
+#         if response_year is None and response_id is None:
+#             return jsonify(Response.query.paginate(page=page, per_page=MAX_RESULTS_PER_PAGE, error_out=False).items)
+#         elif response_year is not None and response_id is None:
+#             return jsonify(Response.query.filter_by(response_year=response_year).paginate(page=page, per_page=MAX_RESULTS_PER_PAGE, error_out=False).items)
+#         elif response_year is not None and response_id is not None:
+#             return jsonify(
+#                 Response.query.filter_by(response_year=response_year, response_id=response_id).first_or_404())
+#         return jsonify({'error': 'Invalid request'}), 400
+#
+#
+# # Requires passing in JSON data
+# app.add_url_rule('/responses/<int:page>', methods=['GET'], view_func=RespondentAnswers.as_view('users'))
+# # Only require the ID for the operation
+# app.add_url_rule('/responses/<int:response_year>/<int:page>', methods=['GET'],
+#                  view_func=RespondentAnswers.as_view('users_year'))
+# app.add_url_rule('/response/<int:response_year>/<int:response_id>', methods=['GET'],
+#                  view_func=RespondentAnswers.as_view('users_year_id'))
 
 if __name__ == '__main__':
     app.run(use_reloader=False)

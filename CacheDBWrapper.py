27835fe29c42e62ac974f05e7d2fc3174eea0b7c
Chong Jin Yi <chong601.13@gmail.com>
2021-06-17 09:56:34 +0800

    Optimization: Close opened session after query - Fix timeouts after huge bursts of query due to query not releasing session causing the pool to run out

--- CacheDBWrapper.py
+++ CacheDBWrapper.py
@@ -1,10 +1,11 @@
-from ExampleStackOverflowRest import Response, db_cache
+from ExampleStackOverflowRest import Response, db_cache, db
 
 
 def get_responses_by_page(req, page_number, size_per_page):
     if db_cache.check(req):
         return db_cache.get(req)
-    result = Response.query.paginate(page=page_number, per_page=size_per_page, error_out=False).items
+    result = db.session.query(Response).paginate(page=page_number, per_page=size_per_page, error_out=False).items
+    db.session.remove()
     db_cache.insert(req, result)
     return result
 
@@ -12,8 +13,9 @@ def get_responses_by_page(req, page_number, size_per_page):
 def get_responses_by_year_per_page(req, year, page_number, size_per_page):
     if db_cache.check(req):
         return db_cache.get(req)
-    result = Response.query.filter_by(response_year=year).paginate(page=page_number, per_page=size_per_page,
-                                                                   error_out=False).items
+    result = db.session.query(Response).filter_by(response_year=year).paginate(page=page_number, per_page=size_per_page,
+                                                                               error_out=False).items
+    db.session.remove()
     db_cache.insert(req, result)
     return result
 
@@ -21,7 +23,8 @@ def get_responses_by_year_per_page(req, year, page_number, size_per_page):
 def get_response_by_response_id(req, response_id):
     if db_cache.check(req):
         return db_cache.get(req)
-    result = Response.query.filter_by(response_id=response_id).all()
+    result = db.session.query(Response).filter_by(response_id=response_id).all()
+    db.session.remove()
     db_cache.insert(req, result)
     return result
 
@@ -29,7 +32,8 @@ def get_response_by_response_id(req, response_id):
 def get_response_by_year_respondent_id(req, year, respondent_id):
     if db_cache.check(req):
         return db_cache.get(req)
-    result = Response.query.filter_by(response_year=year, respondent_id=respondent_id).all()
+    result = db.session.query(Response).filter_by(response_year=year, respondent_id=respondent_id).all()
+    db.session.remove()
     db_cache.insert(req, result)
     return result
 
